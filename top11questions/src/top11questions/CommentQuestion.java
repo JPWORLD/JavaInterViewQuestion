3bad93a921bf576d4eb7005f03f51b6c4c56acc8
JPWORLD <jaiprakash05@live.com>
2018-05-06 14:04:36 +0530

    firstCommit
    firstCommit

---
+++ top11questions/src/top11questions/CommentQuestion.java
@@ -0,0 +1,11 @@
+package top11questions;
+
+public class CommentQuestion {
+
+	public static void main(String[] args) {
+
+	//this is the comment line but how the line is getting executed \u000d	System.out.println("command executed");
+		
+	}
+
+}
---
+++ top11questions/src/top11questions/StringIntegerConcatination.java
@@ -0,0 +1,10 @@
+package top11questions;
+
+public class StringIntegerConcatination {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
