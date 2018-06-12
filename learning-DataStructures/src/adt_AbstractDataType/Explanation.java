9192ac364da38f47d9c7e8c710e8ee170487083b
JPWORLD <jaiprakash05@live.com>
2018-06-12 09:03:12 +0530

    new updated list
    new updated list

---
+++ learning-DataStructures/src/adt_AbstractDataType/Explanation.java
@@ -0,0 +1,8 @@
+package adt_AbstractDataType;
+
+public class Explanation {
+	//data type is set of values which runs operations
+	//Primitive : cause its predefined by the java 
+	//Abstract data type gets created using instance
+
+}
--- practicalDataStructureAndAlgorithm/src/prerequisiteIntroToObjectOrientedProgramming01/ClassObject.java
+++ practicalDataStructureAndAlgorithm/src/prerequisiteIntroToObjectOrientedProgramming01/ClassObject.java
@@ -7,6 +7,32 @@ public class ClassObject {
 	//A Class is a specification of the blue print of the object
 	//constructors intended to provide instruction that how the object of that class should be constructed 
 	//Objects Exists During Application Runtime
+	//JVM looks for the main method 
+	
+	//Momory Area
+	
+	//Stack
+		// hit the run button : main method get invoked by stack memory area
+			//local variable is also gets counted in stack
+			//
+	//Heap
+		//Objects resides in the Heap 
+			car myCar;//Reference variable 
+			myCar = new Car();//object variable 
+			
+	//Garbage Collection
+		//whenever the new object is created the another refrence object will be
+		//wiped off
+		//always Handles the Heap Area.
+	
+	//Instance Variable
+		//A variable belongs to a particular instance.
+		// it is use to maintaine the instance of that particular variable object
+			Car myCar = new Car(); 
+	
+	
+	
+	
 }
 
 //java class file structure 
